f53561daaa56aa97b33157cb2a91c19009156daa
Sean Murphy <sean@iamseanmurphy.com>
2024-10-01 06:13:42 +0000

    2.8.3

---
+++ falcon/falcon.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Plugin Name: Falcon
+ * Plugin URI:  https://elightup.com
+ * Description: WordPress optimizations & tweaks
+ * Version:     2.8.3
+ * Author:      eLightUp
+ * Author URI:  https://elightup.com
+ * License:     GPL2+
+ * Text Domain: falcon
+ * Domain Path: /languages/
+ */
+
+namespace Falcon;
+
+define( 'FALCON_URL', plugin_dir_url( __FILE__ ) );
+define( 'FALCON_DIR', __DIR__ );
+
+require __DIR__ . '/vendor/autoload.php';
+
+new Settings;
+
+new General;
+new Admin;
+new Security;
+new Email;
+
+if ( is_admin() ) {
+	new Core;
+} else {
+	new Header;
+	new Media;
+	new LazyLoadCSS;
+}
---
+++ falcon/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'elightup/falcon',
+        'pretty_version' => '2.8.3',
+        'version' => '2.8.3.0',
+        'reference' => 'a4666b90b20e698f51aea81622ad3f1e0c35353f',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'elightup/falcon' => array(
+            'pretty_version' => '2.8.3',
+            'version' => '2.8.3.0',
+            'reference' => 'a4666b90b20e698f51aea81622ad3f1e0c35353f',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
